c36cd0749ce92cb3a1ea9a49859dba7f3ffe6390
Yogi <m6@yogigrantz.com>
2017-10-23 12:05:03 -0700

    Initial check in

---
+++ MultiThread/MultiThread/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MultiThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstSearchResult = new System.Windows.Forms.ListBox();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpenDirDialog = new System.Windows.Forms.Button();
+            this.lstFolders = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(235, 461);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 37);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstSearchResult
+            // 
+            this.lstSearchResult.Enabled = false;
+            this.lstSearchResult.FormattingEnabled = true;
+            this.lstSearchResult.Location = new System.Drawing.Point(235, 35);
+            this.lstSearchResult.Name = "lstSearchResult";
+            this.lstSearchResult.Size = new System.Drawing.Size(264, 420);
+            this.lstSearchResult.TabIndex = 1;
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPattern.Location = new System.Drawing.Point(178, 35);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(51, 26);
+            this.txtPattern.TabIndex = 2;
+            this.txtPattern.Text = "*.tab";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pattern";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Folder";
+            // 
+            // btnOpenDirDialog
+            // 
+            this.btnOpenDirDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenDirDialog.Location = new System.Drawing.Point(180, 67);
+            this.btnOpenDirDialog.Name = "btnOpenDirDialog";
+            this.btnOpenDirDialog.Size = new System.Drawing.Size(49, 38);
+            this.btnOpenDirDialog.TabIndex = 5;
+            this.btnOpenDirDialog.Text = "...";
+            this.btnOpenDirDialog.UseVisualStyleBackColor = true;
+            this.btnOpenDirDialog.Click += new System.EventHandler(this.btnOpenDirDialog_Click);
+            // 
+            // lstFolders
+            // 
+            this.lstFolders.FormattingEnabled = true;
+            this.lstFolders.Location = new System.Drawing.Point(42, 113);
+            this.lstFolders.Name = "lstFolders";
+            this.lstFolders.Size = new System.Drawing.Size(187, 342);
+            this.lstFolders.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(402, 461);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Search Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 526);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstFolders);
+            this.Controls.Add(this.btnOpenDirDialog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.lstSearchResult);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Multi-threaded Search File Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstSearchResult;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOpenDirDialog;
+        private System.Windows.Forms.ListBox lstFolders;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MultiThread/MultiThread/Form1.cs
@@ -0,0 +1,64 @@
+﻿using DelegateAndEvents;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MultiThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (this.lstFolders.Items.Count > 0 && this.txtPattern.Text.Contains("*."))
+            {
+                List<string> folders = new List<string>();
+                foreach (string s in this.lstFolders.Items)
+                    folders.Add(s);
+
+                MTFileSearch.OnMTFilesFound += new DGGetResult(getEventRunningInClient);
+                MTFileSearch.Search(folders.ToArray(), this.txtPattern.Text);
+            }
+        }
+
+        private void getEventRunningInClient(string[] result)
+        {
+            if (this.lstSearchResult.InvokeRequired)
+            {
+                DGGetResult g = new DGGetResult(PopulateListBox);
+                this.Invoke(g, new object[] { result });
+            }
+            else
+            {
+                PopulateListBox(result);
+            }
+        }
+
+        private void PopulateListBox(string[] result)
+        {
+            foreach (string s in result)
+            {
+                lstSearchResult.Items.Add(s);
+                lstSearchResult.Refresh();
+            }
+        }
+
+        private void btnOpenDirDialog_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fb = new FolderBrowserDialog())
+            {
+                fb.ShowDialog();
+                if (fb.SelectedPath != null && !this.lstFolders.Items.Contains(fb.SelectedPath))
+                {
+                    this.lstFolders.Items.Add(fb.SelectedPath);
+                    this.btnSearch.Enabled = true;
+                    this.lstSearchResult.Enabled = true;
+                }
+            }
+        }
+    }
+}
